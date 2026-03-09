373bb2010f002fb306a8db88287f86f06940235c
Charles Pletcher <cpletcher2@gmail.com>
2026-03-09 12:37:42 -0400

    Add alignment script for sec00009

---
+++ commentaries/scripts/align_sec00009_to_phi0474.py
@@ -0,0 +1,147 @@
+import json
+import os
+
+from dataclasses import asdict, dataclass
+from pathlib import Path
+
+from lxml import etree
+
+DIR = Path(os.getenv("PDLREFWK_ROOT")).resolve() / "data" / "sec00009"
+NAMESPACES = {
+    "tei": "http://www.tei-c.org/ns/1.0",
+    "ti": "http://chs.harvard.edu/xmlns/cts",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+}
+SUBDIRS = [d for d in DIR.iterdir() if d.is_dir()]
+
+
+@dataclass
+class Citation:
+    urn: str
+    quote: str
+    ref: str
+    ref_urn: str
+
+
+@dataclass
+class Gloss:
+    """
+    This class holds information for each gloss,
+    and provides a convenient method for converting
+    to JSON.
+
+    @urn: The CTS URN in the commentary
+    @corresp: The CTS URN in the base text
+    @content: The content of the <gloss> XML tag
+    @citations: A list of loci paralleli cited in the gloss
+    @lemma: The bare lemma of @corresp citation
+    """
+
+    urn: str
+    corresp: str
+    content: str
+    citations: list[Citation]
+    lemma: str
+
+
+def read_file(path: Path) -> etree.ElementTree:
+    return etree.parse(path)
+
+
+def collect_glosses(tei_file: Path, work_urn_fragment: str, commentary_base_urn: str):
+    tree = read_file(tei_file)
+
+    glosses: list[Gloss] = []
+
+    for textpart in tree.iterfind(
+        ".//tei:div[@subtype='section']", namespaces=NAMESPACES
+    ):
+        for s_el in textpart.iterfind(".//tei:s", namespaces=NAMESPACES):
+            lemma = etree.tostring(s_el, method="text", encoding="unicode").strip()
+            ana = s_el.get("ana")
+            xpath = f".//tei:gloss[@xml:id='{ana.replace("#", "")}']"
+            gloss = textpart.find(xpath, namespaces=NAMESPACES)
+
+            assert gloss is not None, f"No gloss found for {ana} in {work_urn_fragment}"
+
+            phi0474_urn = None
+            textpart_corresp = textpart.get("corresp")
+
+            if textpart_corresp is not None:
+                if textpart_corresp.startswith(work_urn_fragment):
+                    phi0474_urn = textpart_corresp
+                else:
+                    print(
+                        f"Incomplete @corresp attribute on {etree.tostring(textpart)}."
+                    )
+
+                    maybe_corresp = f"{work_urn_fragment}:{textpart_corresp}"
+
+                    print(f"Does this look correct? {maybe_corresp}")
+                    user_response = input(
+                        "Type 'y' to accept; otherwise, enter the URN manually.\n\n"
+                    )
+
+                    if user_response.lower() == "y":
+                        phi0474_urn = maybe_corresp
+                    else:
+                        phi0474_urn = user_response
+            else:
+                textpart_n = textpart.get("n")
+                phi0474_urn = f"{urn_prefix}:{textpart_n}"
+
+            glosses.append(
+                Gloss(
+                    urn=f"{commentary_base_urn}:{len(glosses) + 1}",
+                    corresp=phi0474_urn,
+                    content=etree.tostring(
+                        gloss, xml_declaration=False, encoding="unicode"
+                    ),
+                    citations=[],
+                    lemma=lemma,
+                )
+            )
+
+    return glosses
+
+
+def get_about_urn(cts_file: Path):
+    tree = read_file(cts_file)
+
+    about_el = tree.find(".//ti:about", namespaces=NAMESPACES)
+
+    return about_el.get("urn")
+
+
+APPROVED_WORK_FRAGMENTS = ["sec002", "sec003a"]
+
+OUT_ROOT = Path(__file__).resolve().parent.parent.parent / "test-data" / "commentaries"
+
+
+def main():
+    for d in SUBDIRS:
+        # sec001 is the introduction, which we
+        # don't need to align
+        if d.name not in APPROVED_WORK_FRAGMENTS:
+            continue
+        cts_file = d / "__cts__.xml"
+        tei_file = d / f"sec00009.{d.name}.perseus-eng1.xml"
+
+        about_urn = get_about_urn(cts_file)
+        commentary_urn = f"urn:cts:latinLit:sec00009.{d.name}.perseus-eng1"
+
+        glosses = collect_glosses(tei_file, about_urn, commentary_urn)
+
+        outdir = OUT_ROOT / f"sec00009.{d.name}.perseus-eng1"
+
+        outdir.mkdir(exist_ok=True)
+
+        outfile = outdir / f"glossae_001.jsonl"
+
+        with open(outfile, "w", newline="") as f:
+            for gloss in glosses:
+                print(json.dumps(asdict(gloss), ensure_ascii=False), file=f)
+
+
+if __name__ == "__main__":
+    main()
